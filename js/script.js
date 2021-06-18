3d193c93118a4ac5ef587f57b5fb5bc416f48e3c
Momosan09 <80794773+Momosan09@users.noreply.github.com>
2021-06-18 15:23:39 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-
-function convert() {
-    let celsius = parseFloat(document.getElementById('tc').value);
-    let fahrenheit = ((celsius * (9/5)) + 32).toFixed(2);
-    let kelvin = (celsius + 273.15).toFixed(2);
-    document.getElementById('tk').innerHTML = kelvin;
-    document.getElementById('tf').innerHTML = fahrenheit;
-  }
